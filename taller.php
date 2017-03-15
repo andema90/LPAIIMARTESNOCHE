2bb82dee809b683827271b3779f6ccc0094de205
Mauricio Zuluaga <andema90@hotmail.com>
2017-03-14 19:30:01 -0500

    se agregaron comoentarios al codigo

--- taller.php
+++ taller.php
@@ -4,16 +4,18 @@
      $i=0;
 	 $j=0;
 	 $esPrimo=0; 
-
+	 //primer for
      for($i = 2; $i <= 100; $i++) 
      { 
          $esPrimo = 1; 
+		 //segundo for
           for($j = 2; $j < $i; $j ++) {
                if(!($i % $j)) 
                { 
                     $esPrimo = 0;  
                } 
           }
+		  //if para imprimir si el numero es primo
 		  if($esPrimo) {
                echo $i."<br>"; 
 	      }
